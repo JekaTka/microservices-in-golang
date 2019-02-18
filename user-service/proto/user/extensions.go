ff95b3549631e26d02235e2a4ac82f6c7d4b2ad2
Jenya Tkachenko <jekatka@gmail.com>
2019-02-18 20:28:18 +0000

    CLI

---
+++ user-service/proto/user/extensions.go
@@ -0,0 +1,10 @@
+package go_micro_srv_user
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+func (model *User) BeforeCreate(scope *gorm.Scope) error {
+	uuid := uuid.NewV4()
+	return scope.SetColumn("Id", uuid.String())
+}
